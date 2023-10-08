128583f0d4d068598258a53d79c0979588c170e6
Manmohan_Sharma <manmohansharma4854@gmail.com>
2023-10-08 10:19:33 +0530

    CRUD API Operations

---
+++ gobank/api.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
+
+// APIServer represents an API server.
+// It may be injected as a dependency and may contain information such as a database connection,
+// an HTTP client, AWS information, etc.
+type APIServer struct {
+	addr    string   // The server address.
+	storage *Storage // Database connection or GORM instance. Storage layer or use directly db   *gorm.DB
+	// Additional fields for HTTP client, AWS information, etc.
+}
+
+func newAPIServer(addr string, db *gorm.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		storage: &Storage{
+			db: db,
+		}, // db connection is passed as dependency injected so that we come to know
+		// its existence in advance rather than reported by client from the front-end.
+	}
+}
+
+// Every server must have a Run()
+func (s *APIServer) Run() { // Dependency injection via receiver argument
+	//migrate(): to create table schemas in DB, to be run once in the beginning
+	s.migrate()
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/welcome", func(w http.ResponseWriter, r *http.Request) {
+		s.handleGreet(w, r)
+	})
+	router.HandleFunc("/post", s.handleGetPost)
+	router.HandleFunc("/post/createPayload", s.handleCreatePost)
+	//s.interruptDemo()
+
+	fmt.Printf("Server starting on address %s", s.addr)
+	http.ListenAndServe(s.addr, router) //To Start the Server
+}
+
+// migrate(): to create table schemas in DB, to be run once in the beginning
+func (s *APIServer) migrate() {
+	// Migrate the schema
+	s.storage.db.AutoMigrate(&Post{})
+}
---
+++ gobank/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	//fmt.Println("=============Jai Jai RadhaKrishn========")
+	//db before the server
+	dbUrl := "postgres://postgres:postgres@localhost:5430/gobank" //avoiding to use 5432 port as there can be some conflict with already running db on default port
+	//gobank is the database name
+	db, err := getDB(dbUrl)
+	if err != nil {
+		panic("failed to connect to the database")
+	}
+
+	addr := ":3024"
+	server1 := newAPIServer(addr, db) //server2:= newAPIServer(":4000")
+	server1.Run()
+
+}
+
+/*
+Receiver receives the dependencies, that is why its name, to provide dependency injection.
+*/

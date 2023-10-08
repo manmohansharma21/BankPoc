9c329731de75234ef4e0800d99d86c07728b0677
Manmohan_Sharma <manmohansharma4854@gmail.com>
2023-10-08 11:42:43 +0530

    APIs handling DB CRUD

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 		panic("failed to connect to the database")
 	}
 
-	addr := ":3029"
+	addr := ":3028"
 	server1 := newAPIServer(addr, db) //server2:= newAPIServer(":4000")
 	server1.Run()
 
